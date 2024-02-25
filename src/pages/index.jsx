06cd5c0490ab27537d824d1dcd7647633bf9f0b9
Aleksandar Petkov <iskander9207@icloud.com>
2024-02-25 16:15:46 +0200

    created example button to test styled-components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import { ExampleButton } from "../components/Button";
 
 export default function Home() {
     return (
         <Layout>
+            <ExampleButton>Test Button</ExampleButton>
             <h1>Hello people!</h1>
         </Layout>
     );
