085048254048978b7f2851b8b650cd7263a1e78d
Aleksandar Petkov <iskander9207@icloud.com>
2024-02-26 18:22:15 +0200

    Update gatsby-config.js
    setup siteMetaData in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,21 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipe site",
+    author: "@AlexanderPetkov",
+    person: { name: "Alexander", age: 31 },
+    simpleData: ['item1', 'item2'],
+    complexData: [
+      { name: "Alexander", age: 31 }, { name: "John", age: 34 },
+    ]
+  },
   plugins:
-    [`gatsby-plugin-styled-components`, `gatsby-plugin-image`,
+    [
+      `gatsby-plugin-styled-components`,
+      `gatsby-plugin-image`,
       `gatsby-plugin-sharp`,
-      `gatsby-transformer-sharp`,],
+      `gatsby-transformer-sharp`,
+    ],
 }
