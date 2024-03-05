f931b72b64d5367d8a3e758427f66b345802defd
Aleksandar Petkov <iskander9207@icloud.com>
2024-03-05 11:23:52 +0200

    rendered the tags

--- src/components/TagList.jsx
+++ src/components/TagList.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import setupTags from "../utils/setupTags";
+import { Link } from "gatsby";
 
-const TagList = () => {
+const TagList = ({ recipes }) => {
+    const newTags = setupTags(recipes);
+
     return (
-        <div>
-            <h4>this is tags list</h4>
+        <div className="tag-container">
+            <h4>recipes</h4>
+            <div className="tags-list">
+                {newTags.map((tag, index) => {
+                    const [text, value] = tag;
+
+                    return (
+                        <Link key={index} to={`/${text}`}>
+                            {text} ({value})
+                        </Link>
+                    );
+                })}
+            </div>
         </div>
     );
 };
